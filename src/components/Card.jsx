57a46bf59dec9c2224cc066e69ee0bfc50a1d6b6
Lisa Johansson <lisa.jn@hotmail.com>
2024-01-18 18:02:46 +0100

    css card

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -3,10 +3,10 @@
 function Card(){
     return (
         <div className="card">
-            <img src="" alt="shop pic" height={250}></img>
+            <img src="" alt="shop image" height={250}></img>
             <h2>Produkt Namn</h2>
-            <p>Produkt beskrivning</p>
-            <button className='btn'>Lägg till i varukorgen</button>
+            <p>Produkt pris</p>
+            <button className='btn'>Buy</button>
         </div>
     );
 
