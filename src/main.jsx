0d098995201ffdfb295631f0a6fef8a6f0aecb79
Lisa Johansson <lisa.jn@hotmail.com>
2024-01-24 14:12:00 +0100

    css

--- src/main.jsx
+++ src/main.jsx
@@ -1,8 +1,8 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
-import './index.css'
 import './style/Product.css'
+import './style/Globle.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
