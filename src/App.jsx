b8bbdd390112f0e2efdd18cd5bb6e794cd7eba8a
Lisa Johansson <lisa.jn@hotmail.com>
2024-01-31 11:47:39 +0100

    link fix

--- src/App.jsx
+++ src/App.jsx
@@ -19,7 +19,7 @@ function App() {
     <Router>
       <Navbar />
       <Routes>
-          <Route path="/Home" element={<Home />} />
+          <Route path="/" element={<Home />} />
           {/*Använder '/' som sökväg för hemsidan istället för "/Home" för att få slideshow att fungera*/}
           <Route path="/about" element={<About />} />
           <Route path="/products" element={<Products />} />
