f14f2e09524acdd19548ed6e28c9d7fc75f7ab4d
Mzn97 <mazen.karim97@yahoo.com>
2024-02-02 01:58:58 +0100

    Modifying footer & Information page

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -7,28 +7,65 @@ const Footer = () => {
     <footer className="footer-container">
       <div className="footer-content">
         <div className="footer-section">
-          <h4>About Us</h4>
-          <p>
-            "MLNK is your go-to source for premium soccer jerseys, footballs, and a lot more. Elevate your soccer experience with our authentic gear, combining quality and style. Explore the best in the game at MLNK."
-          </p>
+          <h4>Connect with MLNK</h4>
+          <p></p>
+          <svg xmlns="http://www.w3.org/2000/svg" className="icon icon-tabler icon-tabler-brand-instagram" width="39" height="44" viewBox="0 0 24 24" strokeWidth="1.5" stroke="#666666" fill="none" strokeLinecap="round" strokeLinejoin="round">
+            <path stroke="none" d="M0 0h24v24H0z" fill="none"/>
+            <path d="M4 4m0 4a4 4 0 0 1 4 -4h8a4 4 0 0 1 4 4v8a4 4 0 0 1 -4 4h-8a4 4 0 0 1 -4 -4z" />
+            <path d="M12 12m-3 0a3 3 0 1 0 6 0a3 3 0 1 0 -6 0" />
+            <path d="M16.5 7.5l0 .01" />
+          </svg>
+          <svg xmlns="http://www.w3.org/2000/svg" className="icon icon-tabler icon-tabler-brand-facebook" width="37" height="44" viewBox="0 0 24 24" strokeWidth="1.5" stroke="#666666" fill="none" strokeLinecap="round" strokeLinejoin="round">
+            <path stroke="none" d="M0 0h24v24H0z" fill="none"/>
+            <path d="M7 10v4h3v7h4v-7h3l1 -4h-4v-2a1 1 0 0 1 1 -1h3v-4h-3a5 5 0 0 0 -5 5v2h-3" />
+          </svg>
+          <svg xmlns="http://www.w3.org/2000/svg" className="icon icon-tabler icon-tabler-brand-x" width="37" height="44" viewBox="0 0 24 24" strokeWidth="1.5" stroke="#666666" fill="none" strokeLinecap="round" strokeLinejoin="round">
+            <path stroke="none" d="M0 0h24v24H0z" fill="none"/>
+            <path d="M4 4l11.733 16h4.267l-11.733 -16z" />
+            <path d="M4 20l6.768 -6.768m2.46 -2.46l6.772 -6.772" />
+          </svg>
+          <p></p>
+          <svg xmlns="http://www.w3.org/2000/svg" className="icon icon-tabler icon-tabler-brand-tiktok" width="37" height="44" viewBox="0 0 24 24" strokeWidth="1.5" stroke="#666666" fill="none" strokeLinecap="round" strokeLinejoin="round" transform="translate(-5, 0)">
+            <path stroke="none" d="M0 0h24v24H0z" fill="none"/>
+            <path d="M21 7.917v4.034a9.948 9.948 0 0 1 -5 -1.951v4.5a6.5 6.5 0 1 1 -8 -6.326v4.326a2.5 2.5 0 1 0 4 2v-11.5h4.083a6.005 6.005 0 0 0 4.917 4.917z" />
+          </svg>
+          <svg xmlns="http://www.w3.org/2000/svg" className="icon icon-tabler icon-tabler-brand-youtube" width="37" height="44" viewBox="0 0 24 24" strokeWidth="1.5" stroke="#666666" fill="none" strokeLinecap="round" strokeLinejoin="round" transform="translate(1, 0)">
+            <path stroke="none" d="M0 0h24v24H0z" fill="none"/>
+            <path d="M2 8a4 4 0 0 1 4 -4h12a4 4 0 0 1 4 4v8a4 4 0 0 1 -4 4h-12a4 4 0 0 1 -4 -4v-8z" />
+            <path d="M10 9l5 3l-5 3z" />
+          </svg>
+          <svg xmlns="http://www.w3.org/2000/svg" className="icon icon-tabler icon-tabler-brand-pinterest" width="37" height="44" viewBox="0 0 24 24" strokeWidth="1.5" stroke="#666666" fill="none" strokeLinecap="round" strokeLinejoin="round" transform="translate(6, 0)">
+            <path stroke="none" d="M0 0h24v24H0z" fill="none"/>
+            <path d="M8 20l4 -9" />
+            <path d="M10.7 14c.437 1.263 1.43 2 2.55 2c2.071 0 3.75 -1.554 3.75 -4a5 5 0 1 0 -9.7 1.7" />
+            <path d="M12 12m-9 0a9 9 0 1 0 18 0a9 9 0 1 0 -18 0" />
+          </svg>
         </div>
 
         <div className="footer-section">
           <h4>Quick Links</h4>
           <ul className="footer-links">
-            <li><a href="/Information"> information</a></li>
+            <li><Link to="/Information" style={{ color: '#666666', fontSize: '19px', marginTop: '20px', display: 'block' }}>Information</Link></li>
           </ul>
         </div>
 
         <div className="footer-section">
-          <h4>Contact</h4>
-          <p>Email: info@MLNK.com</p>
-          <p>Phone: +123 456 789</p>
+          <h4 style={{ lineHeight: '1', marginBottom: '5px' }}>
+            Contact Us 
+            <svg xmlns="http://www.w3.org/2000/svg" className="icon icon-tabler icon-tabler-device-mobile" width="44" height="30" viewBox="0 0 24 24" strokeWidth="1.5" stroke="#000000" fill="none" strokeLinecap="round" strokeLinejoin="round" transform="translate(-6, 5)" > 
+              <path stroke="none" d="M0 0h24v24H0z" fill="none"/>
+              <path d="M6 5a2 2 0 0 1 2 -2h8a2 2 0 0 1 2 2v14a2 2 0 0 1 -2 2h-8a2 2 0 0 1 -2 -2v-14z" />
+              <path d="M11 4h2" />
+              <path d="M12 17v.01" />
+            </svg>
+          </h4>
+          <p style={{ marginTop: '10px' }}>Email: info@MLNK.com</p>
+          <p style={{ marginTop: '10px' }}>Phone: +123 456 789</p>
         </div>
       </div>
 
       <div className="footer-bottom">
-        <p>&copy; {new Date().getFullYear()} MLNK </p>
+        <p style={{ fontSize: '14px' }}>&copy; {new Date().getFullYear()} Sweden MLNK, Inc. All Rights Reserved </p>
       </div>
     </footer>
   );
--- src/pages/Information.jsx
+++ src/pages/Information.jsx
@@ -1,12 +1,75 @@
 import React from 'react';
+import '../style/Information.css';
 
 const Information = () => {
+  return (
+    <div>
+      <h1>Information</h1>
 
-    return(
-        <div>
-            <h1>information TEST</h1>
-        </div>
-    )
-}
+      <section>
+        <h2>Delivery </h2>
 
-export default Information
+        <svg xmlns="http://www.w3.org/2000/svg" className="icon icon-tabler icon-tabler-truck-delivery" width="100" height="65" viewBox="0 0 24 24" strokeWidth="1.5" stroke="#555" fill="none" strokeLinecap="round" strokeLinejoin="round">
+          <path stroke="none" d="M0 0h24v24H0z" fill="none" />
+          <path d="M7 17m-2 0a2 2 0 1 0 4 0a2 2 0 1 0 -4 0" />
+          <path d="M17 17m-2 0a2 2 0 1 0 4 0a2 2 0 1 0 -4 0" />
+          <path d="M5 17h-2v-4m-1 -8h11v12m-4 0h6m4 0h2v-6h-8m0 -5h5l3 5" />
+          <path d="M3 9l4 0" />
+        </svg>
+
+        <p>We provide FREE STANDARD SHIPPING on all orders.</p>
+        <p>Our efficient delivery system ensures that your chosen football gear is dispatched promptly to your doorstep. Whether you're upgrading your cleats, grabbing the latest jerseys, or getting ready for a new season, we've got you covered.</p>
+        <p>Our shipping typically takes between 3 - 5 days for delivery of all items.</p>
+      </section>
+
+      <section>
+        <h2>Exchanges & Returns</h2>
+        <svg xmlns="http://www.w3.org/2000/svg" className="icon icon-tabler icon-tabler-truck-return" width="100" height="65" viewBox="0 0 24 24" strokeWidth="1.5" stroke="#555" fill="none" strokeLinecap="round" strokeLinejoin="round">
+          <path stroke="none" d="M0 0h24v24H0z" fill="none" />
+          <path d="M7 17m-2 0a2 2 0 1 0 4 0a2 2 0 1 0 -4 0" />
+          <path d="M17 17m-2 0a2 2 0 1 0 4 0a2 2 0 1 0 -4 0" />
+          <path d="M5 17h-2v-11a1 1 0 0 1 1 -1h9v6h-5l2 2m0 -4l-2 2" />
+          <path d="M9 17l6 0" />
+          <path d="M13 6h5l3 5v6h-2" />
+        </svg>
+        <p>We are committed to ensuring your satisfaction with every kick, pass, and goal. In the rare event that you need to exchange or return your football equipment, we've got you covered with our hassle-free process.</p>
+
+        <h4 style={{ color: '#666666' }}>We offer our customer ▼  </h4>
+        <p>• Delivery Assurance </p>
+        <p>• Simple Exchanges </p>
+        <p>• Easy Returns </p>
+      </section>
+
+      <section>
+        <h2>Purchase Terms</h2>
+        <svg xmlns="http://www.w3.org/2000/svg" className="icon icon-tabler icon-tabler-file-description" width="100" height="65" viewBox="0 0 24 24" strokeWidth="1.5" stroke="#555" fill="none" strokeLinecap="round" strokeLinejoin="round">
+          <path stroke="none" d="M0 0h24v24H0z" fill="none" />
+          <path d="M14 3v4a1 1 0 0 0 1 1h4" />
+          <path d="M17 21h-10a2 2 0 0 1 -2 -2v-14a2 2 0 0 1 2 -2h7l5 5v11a2 2 0 0 1 -2 2z" />
+          <path d="M9 17h6" />
+          <path d="M9 13h6" />
+        </svg>
+        <p>Here are our Purchase Terms to ensure you enjoy every moment on the field ▼ </p>
+        <p>• Swift Delivery </p>
+        <p>• Quality Assurance </p>
+        <p>• Effortless Ordering </p>
+      </section>
+
+      <section>
+        <h2>Purpose</h2>
+        <svg xmlns="http://www.w3.org/2000/svg" className="icon icon-tabler icon-tabler-trophy" width="65" height="100" viewBox="0 0 24 24" strokeWidth="1.5" stroke="#555" fill="none" strokeLinecap="round" strokeLinejoin="round">
+          <path stroke="none" d="M0 0h24v24H0z" fill="none" />
+          <path d="M8 21l8 0" />
+          <path d="M12 17l0 4" />
+          <path d="M7 4l10 0" />
+          <path d="M17 4v8a5 5 0 0 1 -10 0v-8" />
+          <path d="M5 9m-2 0a2 2 0 1 0 4 0a2 2 0 1 0 -4 0" />
+          <path d="M19 9m-2 0a2 2 0 1 0 4 0a2 2 0 1 0 -4 0" />
+        </svg>
+        <p>Changing the game starts with those that play it. We work with athletes, communities, and organizations around the world that are challenging the status quo, championing diversity, and bringing people together. All with the goal of creating a better future for everyone.</p>
+      </section>
+    </div>
+  );
+};
+
+export default Information;
