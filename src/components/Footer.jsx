f46feb3326df6444ee803616387f096f97abd2e1
Mzn97 <mazen.karim97@yahoo.com>
2024-02-02 02:25:53 +0100

    Modifying footer & Information page

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -45,27 +45,19 @@ const Footer = () => {
         <div className="footer-section">
           <h4>Quick Links</h4>
           <ul className="footer-links">
-            <li><Link to="/Information" style={{ color: '#666666', fontSize: '19px', marginTop: '20px', display: 'block' }}>Information</Link></li>
+            <li><Link to="/Information" className="information-link">Information</Link></li>
           </ul>
         </div>
 
         <div className="footer-section">
-          <h4 style={{ lineHeight: '1', marginBottom: '5px' }}>
-            Contact Us 
-            <svg xmlns="http://www.w3.org/2000/svg" className="icon icon-tabler icon-tabler-device-mobile" width="44" height="30" viewBox="0 0 24 24" strokeWidth="1.5" stroke="#000000" fill="none" strokeLinecap="round" strokeLinejoin="round" transform="translate(-6, 5)" > 
-              <path stroke="none" d="M0 0h24v24H0z" fill="none"/>
-              <path d="M6 5a2 2 0 0 1 2 -2h8a2 2 0 0 1 2 2v14a2 2 0 0 1 -2 2h-8a2 2 0 0 1 -2 -2v-14z" />
-              <path d="M11 4h2" />
-              <path d="M12 17v.01" />
-            </svg>
-          </h4>
-          <p style={{ marginTop: '10px' }}>Email: info@MLNK.com</p>
-          <p style={{ marginTop: '10px' }}>Phone: +123 456 789</p>
+          <h4>Contact Us</h4>
+          <p>Email: info@MLNK.com</p>
+          <p>Phone: +123 456 789</p>
         </div>
       </div>
 
       <div className="footer-bottom">
-        <p style={{ fontSize: '14px' }}>&copy; {new Date().getFullYear()} Sweden MLNK, Inc. All Rights Reserved </p>
+        <p>&copy; {new Date().getFullYear()} Sweden MLNK, Inc. All Rights Reserved </p>
       </div>
     </footer>
   );
