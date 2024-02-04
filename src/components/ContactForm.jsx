001096e1687b7a69856e0775c473dda4cb5a2277
kbardach <144119408+kbardach@users.noreply.github.com>
2024-02-04 17:43:03 +0100

    seperated components from pages

---
+++ src/components/ContactForm.jsx
@@ -0,0 +1,50 @@
+import React, { useRef } from 'react';
+import emailjs from 'emailjs-com';
+import '../style/ContactForm.css'
+
+const ContactForm = () => {
+    const form = useRef()
+
+    const sendEmail = (e) => {
+        e.preventDefault()
+
+        emailjs.sendForm('service_3ym5b6h', 'template_mp2j04o', form.current, 'LmxFhS-dooDm9Km1G')
+            .then((result) => {
+                console.log(result.text);
+                alert('Your message has been sent!')
+            }, (error) => {
+                console.log(error.text);
+                alert('Something went wrong. Please try again!')
+            })
+    }
+
+    return (
+        <div>
+            <form ref={form} onSubmit={sendEmail} className="contact-form">
+                <label className="form-label">Name</label>
+                <input className="form-input"
+                    type="text" 
+                    name="from_name" 
+                    required 
+                />
+
+                <label className="form-label">Email</label>
+                <input className="form-input"
+                    type="email" 
+                    name="from_email" 
+                    required 
+                />
+
+                <label className="form-label">Message</label>
+                <textarea className="form-textarea" 
+                    name="message" 
+                    required
+                />
+
+                <button type="submit" className="form-button">Send</button>
+            </form>
+        </div>
+    )
+}
+
+export default ContactForm;
--- src/pages/ContactUs.jsx
+++ src/pages/ContactUs.jsx
@@ -1,51 +1,13 @@
-import React, { useRef } from 'react';
-import emailjs from 'emailjs-com';
-import '../style/ContactUs.css'
+import React from 'react';
+import ContactForm from '../components/ContactForm';
 
-const ContactForm = () => {
-    const form = useRef()
-
-    const sendEmail = (e) => {
-        e.preventDefault()
-
-        emailjs.sendForm('service_3ym5b6h', 'template_mp2j04o', form.current, 'LmxFhS-dooDm9Km1G')
-            .then((result) => {
-                console.log(result.text);
-                alert('Your message has been sent!');
-            }, (error) => {
-                console.log(error.text);
-                alert('Something went wrong. Please try again!');
-            })
-    }
-
-    return (
-        <div>
-            <h1 className="contact-form-h1">CONTACT US</h1>
-            <form ref={form} onSubmit={sendEmail} className="contact-form">
-                <label className="form-label">Name</label>
-                <input className="form-input"
-                    type="text" 
-                    name="from_name" 
-                    required 
-                />
-
-                <label className="form-label">Email</label>
-                <input className="form-input"
-                    type="email" 
-                    name="from_email" 
-                    required 
-                />
-
-                <label className="form-label">Message</label>
-                <textarea className="form-textarea" 
-                    name="message" 
-                    required
-                />
-
-                <button type="submit" className="form-button">Send</button>
-            </form>
-        </div>
-    )
+const ContactUs = () => {
+  return (
+    <div>
+        <h1 className="contact-form-h1">CONTACT US</h1>
+            <ContactForm />
+    </div>
+  )
 }
 
-export default ContactForm;
+export default ContactUs;
