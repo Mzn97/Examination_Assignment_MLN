f0d33c1e961b346baef0ef30f612ece2ba17a66d
kbardach <144119408+kbardach@users.noreply.github.com>
2024-02-05 19:57:19 +0100

    fucking fuck fix

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -4,20 +4,17 @@ import '../style/Navbar.css'
 import HamburgerMenu from './HamburgerMenu'
 
 const Navbar = () => {
-    
     return (
-        
         <div className="navbar-container">
             <nav className="navbar">
                 <div className='testnav'>
-                
-                <Link to="/" className="navbar-text">MLNK</Link>
-                <Link to="/about" className="navbar-item">About us</Link>
-                <Link to="/products" className="navbar-item">Products</Link>
-                
-                <div className="navbar-container cart-container">
+                    <Link to="/" className="navbar-text">MLNK</Link>
+                    <Link to="/about" className="navbar-item">About us</Link>
+                    <Link to="/products" className="navbar-item">Products</Link>
+                    <Link to="/ShoppingCart" className="navbar-item">Shopping Cart</Link>
+                </div>
+                <div className="action-container">      {/* tillagd */}
                     <Link to="/ShoppingCart" className="navbar-cart">
-                    <span className="navbar-item">Shopping Cart</span>
                         <svg xmlns="http://www.w3.org/2000/svg" className="icon icon-tabler icon-tabler-shopping-cart" width="30" height="35" viewBox="0 0 24 24" strokeWidth="1.5" stroke="#000000" fill="none" strokeLinecap="round" strokeLinejoin="round">
                             <path stroke="none" d="M0 0h24v24H0z" fill="none"/>
                             <path d="M6 19m-2 0a2 2 0 1 0 4 0a2 2 0 1 0 -4 0" />
@@ -26,10 +23,8 @@ const Navbar = () => {
                             <path d="M6 5l14 1l-1 7h-13" />
                         </svg>
                     </Link>
+                    <HamburgerMenu />
                 </div>
-                </div>
-                <HamburgerMenu />
-
             </nav>
         </div>
     )
--- src/pages/ShoppingCart.jsx
+++ src/pages/ShoppingCart.jsx
@@ -1,10 +1,11 @@
 import React from 'react';
+import '../style/ShoppingCart.css'
 
 const ShoppingCart = () => {
 
     return(
         <div>
-            <h1>KUNDKORG TEST</h1>
+            <h1 className='shopping-cart'>KUNDKORG TEST</h1>
             <a href="/checkout"><button>Go to Checkout</button></a>
             {/*Behövs att fixas!! Pop-up ska komma upp istället för en hel sida*/}
         </div>
