47cbd7507c9c7459f1b2cab1ad9639c0d5236529
Lisa Johansson <lisa.jn@hotmail.com>
2024-02-05 13:29:12 +0100

    navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -9,14 +9,6 @@ const Navbar = () => {
         
         <div className="navbar-container">
             <nav className="navbar">
-                <div className="icon-container">
-                    <svg xmlns="http://www.w3.org/2000/svg" className="icon icon-tabler icon-tabler-ball-football" width="56" height="56" viewBox="0 0 24 24" strokeWidth="1.5" stroke="#000000" fill="none" strokeLinecap="round" strokeLinejoin="round">
-                        <path stroke="none" d="M0 0h24v24H0z" fill="none"/>
-                        <path d="M12 12m-9 0a9 9 0 1 0 18 0a9 9 0 1 0 -18 0" />
-                        <path d="M12 7l4.76 3.45l-1.76 5.55h-6l-1.76 -5.55z" />
-                        <path d="M12 7v-4m3 13l2.5 3m-.74 -8.55l3.74 -1.45m-11.44 7.05l-2.56 2.95m.74 -8.55l-3.74 -1.45" />
-                    </svg>
-                </div>
                 <div className='testnav'>
                 
                 <Link to="/" className="navbar-text">MLNK</Link>
@@ -24,7 +16,7 @@ const Navbar = () => {
                 <Link to="/products" className="navbar-item">Products</Link>
                 
                 <div className="navbar-container cart-container">
-                    <Link to="/ShoppingCart" className="navbar-item">
+                    <Link to="/ShoppingCart" className="navbar-cart">
                     <span className="navbar-item">Shopping Cart</span>
                         <svg xmlns="http://www.w3.org/2000/svg" className="icon icon-tabler icon-tabler-shopping-cart" width="30" height="35" viewBox="0 0 24 24" strokeWidth="1.5" stroke="#000000" fill="none" strokeLinecap="round" strokeLinejoin="round">
                             <path stroke="none" d="M0 0h24v24H0z" fill="none"/>
