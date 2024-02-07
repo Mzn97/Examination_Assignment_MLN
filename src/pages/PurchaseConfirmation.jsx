36b9c3a4d699088fb5034afaea2394a94c9d3387
Lisa Johansson <lisa.jn@hotmail.com>
2024-02-07 12:52:56 +0100

    emailJs, math.ramdom

--- src/pages/PurchaseConfirmation.jsx
+++ src/pages/PurchaseConfirmation.jsx
@@ -4,15 +4,18 @@ import '../style/PurchaseConfirmation.css';
 
 const PurchaseConfirmation = () => {
 
-   // const location = this.props;
-   // const { state } = location;
-    //const email = state ? state.email : null;
+     // Generera ett slumpmässigt ordernummer
+     const orderNumber = Math.floor(Math.random() * 900000) + 100000; // Ger ett nummer mellan 100000 och 999999
 
     return (
         <div className='confirmation-container'>
-            <h2>Purchase Completed</h2>
+            <h2>Purchase Completed</h2> 
             
-            <p>Thank you for your purchase! Your order is being processed.</p>
+            <p>Thank you for shopping at MLNK.</p>
+            <p>We have received your order <strong>{orderNumber}.</strong></p><br></br> 
+            <p> We will send an email with a pickup confirmation</p> 
+            <p> as soon as the order is ready for collection from the store.</p>
+    
         </div>
     );
 }
