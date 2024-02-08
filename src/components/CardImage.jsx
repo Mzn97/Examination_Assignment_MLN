c002571e8d4d5b80f4717f68a451c949b16c1e9a
Nathali <verdejo_nathali@hotmail.com>
2024-02-08 14:53:22 +0100

    adjustment on css cart

--- src/components/CardImage.jsx
+++ src/components/CardImage.jsx
@@ -1,13 +1,19 @@
 import React from "react";
 
 
+
 const CardImage = ({imageUrl}) => {
 
     console.log(imageUrl)
 
     return (
         <div className="card-image">
-            <img src={`http://localhost:${import.meta.env.VITE_PORT}` + imageUrl} alt="CartImg" height={170} width={150}/>
+            <img src={`http://localhost:${import.meta.env.VITE_PORT}` + imageUrl}
+                 alt="CartImg"
+                 height={160}
+                 width={150}
+                 style={{ borderRadius: "50px"}}
+            />
         </div>
     )
 }
